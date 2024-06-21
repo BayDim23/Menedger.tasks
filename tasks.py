9e3acd9b6e2fce7bde7113bc647de4f6c63e173a
BayDim23 <168500336+BayDim23@users.noreply.github.com>
2024-06-21 05:47:13 +0300

    add new code

--- tasks.py
+++ tasks.py
@@ -1,37 +1,35 @@
 class Task():
-
-    def __init__(self, id, description, deadline, status="Не выполнено"):
-        self.id = id
-        self.description = description
-        self.deadline = deadline
-        self.status = status
-
-class TasksMenedger():
     def __init__(self):
         self.tasks = []
 
     def add_task(self, description, deadline):
-        task = Task(description, deadline)
-        self.tasks.append(task)
+        self.tasks.append({"description": description, "deadline": deadline, "status": "не выполнено"})
 
-    def mark_task_done(self, description):
+    def complete_task(self, description):
         for task in self.tasks:
-            if task.description == description:
-                task.status = "выполнено"
+            if task["description"] == description:
+                task["status"] = "выполнено"
+                print(f"Задача {task['description']} выполнена.")
+                return
+        print(f"Задача {description} не найдена.")
 
-    def get_current_tasks(self):
-        current_tasks = [task for task in self.tasks if task.status == "не выполнено"]
-        for task in current_tasks:
-            print(f"Описание задачи: {task.description}, Срок выполнения: {task.deadline}, Статус: {task.status}")
+    def show_tasks(self):
+        print("Список задач")
+        for task in self.tasks:
+            if task['status'] == "не выполнено":
+                print(f"{task['description']} - {task['deadline']}")
 
-task_manager = TasksMenedger()
+t = Task()
+t.add_task("Купить молоко", "25.06.2024")
+t.add_task("Помыть кота", "25.06.2024")
+t.add_task("Помыть посуду", "25.06.2024")
 
-task_manager.add_task("Прозвонить клиентов", "15.05.2024")
-task_manager.add_task("Подготовить отчет", "15.05.2024")
+t.show_tasks()
 
-task_manager.mark_task_done("Прозвонить клиентов")
+t.complete_task("Помыть посуду")
+t.show_tasks()
 
-print("Текущие задачи:")
-task_manager.get_current_tasks()
+
+
 
 
